0e7b9d81599fa40ac6314d4e1a3f28be83296cb0
Ankit N. Khambhati <akhambhati@gmail.com>
2016-07-07 16:58:31 -0400

    Added nmf_optimization function to run quick analysis

---
+++ Codebase/helpers/nmf_optimization.py
@@ -0,0 +1,33 @@
+from __future__ import division
+
+import numpy as np
+
+from ..Common.display import my_display
+from ..Networks.SubgraphDetection.nonnegfac import nmf
+
+def execute(expr_id, path_cfg_matr, param_id, param_alpha, param_beta, param_rank, path_output):
+    my_display("\n -- Processing: {} -- Parameter: {}".format(expr_id, param_id), True)
+    
+    df = np.load(path_cfg_matr)
+        
+    cfg_matr = np.nan_to_num(df['cfg_matr'])
+        
+    # Initialize the factors for NMF
+    fac_subnet = np.random.uniform(low=0, high=1.0,
+                                   size=(param_rank,
+                                         cfg_matr.shape[1]))
+    fac_coef = np.random.uniform(low=0, high=1.0,
+                                 size=(param_rank,
+                                       cfg_matr.shape[0]))
+
+    # Run NMF Algorithm
+    fac_subnet, fac_coef, err = nmf.snmf_bcd(
+        cfg_matr,
+        alpha=param_alpha, beta=param_beta,
+        fac_subnet_init=fac_subnet,
+        fac_coef_init=fac_coef,
+        max_iter=100, verbose=False)
+
+    # Cache the NMF result
+    np.savez("{}/NMF_Optimization.Param_{}.{}.npz".format(path_output, param_id, expr_id),
+             fac_subnet=fac_subnet, fac_coef=fac_coef, err=err)
