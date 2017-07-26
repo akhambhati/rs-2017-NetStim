20e8e03eb2e0c01e3c6726a5b1cf4029a15b2094
Ankit N. Khambhati <akhambhati@gmail.com>
2017-07-26 08:53:00 -0400

    Added SGE code to processes baseline trials

---
+++ Analysis_Notebooks/e01new_b-SGE/e01-Generate_ProcList.py
@@ -0,0 +1,28 @@
+#### ---------------------- Front Matter ------------------------
+
+# Standard I/O imports
+import os
+import sys
+import glob
+import pickle as pkl
+import h5py
+import scipy.io as io
+
+# Data manipulation imports
+import numpy as np
+sys.path.append('/data/jag/akhambhati/hoth_research/Echobase')
+import Echobase
+
+# Set Paths
+path_CoreData = '/data/jag/bassett-lab/akhambhati/CORE.PS_Stim'
+path_PeriphData = '/data/jag/bassett-lab/akhambhati/RSRCH.PS_Stim'
+path_ExpData = path_PeriphData + '/e01-FuncNetw_Baseline'
+
+for path in [path_CoreData, path_PeriphData, path_ExpData]:
+    if not os.path.exists(path):
+        print('Path: {}, does not exist'.format(path))
+        os.makedirs(path)
+#### ---------------------- Front Matter ------------------------
+
+raw_path = glob.glob('{}/Base_Trials/*.mat'.format(path_CoreData))
+pkl.dump(raw_path, open('{}/proc_list.pkl'.format(path_ExpData), 'w'))
