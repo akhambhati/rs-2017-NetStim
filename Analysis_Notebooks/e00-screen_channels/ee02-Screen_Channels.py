baa346da58e2f8a0f2736039b2ac1257ad287892
akhambhati <akhambhati@gmail.com>
2018-01-22 13:41:44 -0500

    Fixed script structure and moved main script contents to main scope

--- Analysis_Notebooks/e00-screen_channels/ee02-Screen_Channels.py
+++ Analysis_Notebooks/e00-screen_channels/ee02-Screen_Channels.py
@@ -29,12 +29,12 @@ location = 'cfn'
 
 if location == 'Hoth':
     sys.path.append('/Users/akhambhati/Developer/hoth_research/Echobase')
-    sys.path.append('/Users/akhambhati/Developer/hoth_research/rs-Net_Stim/Analysis_Notebooks/e00-screen_channels')        
+    sys.path.append('/Users/akhambhati/Developer/hoth_research/rs-Net_Stim/Analysis_Notebooks/e00-screen_channels')
     path_CoreData = '/Users/akhambhati/Remotes/CORE.PS_Stim'
     path_PeriphData = '/Users/akhambhati/Remotes/RSRCH.PS_Stim'
 elif location == 'cfn':
     sys.path.append('/data/jag/akhambhati/hoth_research/Echobase')
-    sys.path.append('/data/jag/akhambhati/hoth_research/rs-Net_Stim/Analysis_Notebooks/e00-screen_channels')            
+    sys.path.append('/data/jag/akhambhati/hoth_research/rs-Net_Stim/Analysis_Notebooks/e00-screen_channels')
     path_CoreData = '/data/jag/bassett-lab/akhambhati/CORE.PS_Stim'
     path_PeriphData = '/data/jag/bassett-lab/akhambhati/RSRCH.PS_Stim'
 else:
@@ -52,27 +52,6 @@ import Echobase
 import handle_bad_channels
 #### ---------------------- Front Matter ------------------------
 
-# Grab the Task ID from QSUB
-sge_task_id = int(os.environ['SGE_TASK_ID'])-1
-
-# Load the file list
-proc_list = pkl.load(open('{}/proc_list.pkl'.format(path_ExpData), 'r'))
-subj_id = proc_list[sge_task_id]
-
-# Run and cache output
-foutput = '{}/Clean_Channels.{}.npz'.format(path_ExpData, subj_id)
-if os.path.exists(foutput):
-    print('Output file already exists: {}'.format(foutput))
-    sys.exit()
-monopolar_id, monopolar_lbl, monopolar_id_artifact, monopolar_lbl_artifact, monopolar_resort_ix = run(subj_id)
-np.savez(foutput,
-         monopolar_id=monopolar_id,
-         monopolar_lbl=monopolar_lbl,
-         monopolar_id_artifact=monopolar_id_artifact,         
-         monopolar_lbl_artifact=monopolar_lbl_artifact,
-         monopolar_resort_ix=monopolar_resort_ix)
-
-
 def run(subj_id):
     # Clean the channels for this subject
     monopolar_id, monopolar_lbl, monopolar_resort_ix = handle_bad_channels.init_channels(subj_id)
@@ -83,6 +62,29 @@ def run(subj_id):
         monopolar_id_artifact[stim_tag] = np.unique(
             handle_bad_channels.find_index_in_list(monopolar_lbl,
                                                    monopolar_lbl_artifact[stim_tag]))
-    
+
     return monopolar_id, monopolar_lbl, monopolar_id_artifact, monopolar_lbl_artifact, monopolar_resort_ix
+
+
+if __name__ == '__main__':
+    # Grab the Task ID from QSUB
+    sge_task_id = int(os.environ['SGE_TASK_ID'])-1
+
+    # Load the file list
+    proc_list = pkl.load(open('{}/proc_list.pkl'.format(path_ExpData), 'r'))
+    subj_id = proc_list[sge_task_id]
+
+    # Run and cache output
+    foutput = '{}/Clean_Channels.{}.npz'.format(path_ExpData, subj_id)
+    if os.path.exists(foutput):
+        print('Output file already exists: {}'.format(foutput))
+        sys.exit()
+    monopolar_id, monopolar_lbl, monopolar_id_artifact, monopolar_lbl_artifact, monopolar_resort_ix = run(subj_id)
+    np.savez(foutput,
+             monopolar_id=monopolar_id,
+             monopolar_lbl=monopolar_lbl,
+             monopolar_id_artifact=monopolar_id_artifact,
+             monopolar_lbl_artifact=monopolar_lbl_artifact,
+             monopolar_resort_ix=monopolar_resort_ix)
+
 "
