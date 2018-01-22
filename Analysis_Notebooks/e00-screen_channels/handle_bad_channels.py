dcdd46b6783b6164889e7e6f3743a8879e1a2767
akhambhati <akhambhati@gmail.com>
2018-01-22 13:47:06 -0500

    added scipy stats package import

--- Analysis_Notebooks/e00-screen_channels/handle_bad_channels.py
+++ Analysis_Notebooks/e00-screen_channels/handle_bad_channels.py
@@ -30,6 +30,7 @@ for path in [path_CoreData, path_PeriphData, path_ExpData]:
 
 # Data manipulation imports
 import numpy as np
+import scipy.stats as stats
 import Echobase
 #### ---------------------- Front Matter ------------------------
 
@@ -45,24 +46,24 @@ def find_index_in_list(ref_list, itm_list):
         ix_list.append(ix[0])
     ix_list = np.array(ix_list)
     return ix_list
-    
-    
+
+
 def init_channels(subject_id):
     df_chan = io.loadmat('{}/Electrode_Info/{}.mat'.format(path_CoreData, subject_id))
-    
+
     # Get the bipolar electrode list and convert it to a monopolar list
     if not (df_chan['electrode_id_bp'].shape == df_chan['electrode_label_bp'].shape):
         raise Exception('Bipolar IDs and Bipolar Labels are mismatched.')
     chan_mp_id, unique_ix = np.unique(df_chan['electrode_id_bp'].reshape(-1), return_index=True)
     chan_mp_lbl = np.array([ee[0] for ee in df_chan['electrode_label_bp'].reshape(-1)])[unique_ix]
     n_chan_mp = len(chan_mp_id)
-    
+
     # Screen visually-identified bad channels
     chan_mp_id = np.intersect1d(chan_mp_id, df_chan['electrode_good'][:, 0])
-    
+
     # Re-sort electrodes in order of chan_mp_id
     if len(np.setdiff1d(chan_mp_id, df_chan['electrode_id'][0, :])) > 0:
-        raise Exception('Monopolar list contains IDs not in master electrode list.')                
+        raise Exception('Monopolar list contains IDs not in master electrode list.')
     resort_ix = find_index_in_list(df_chan['electrode_id'][0, :], chan_mp_id)
 
     return chan_mp_id, chan_mp_lbl, resort_ix
@@ -70,14 +71,14 @@ def init_channels(subject_id):
 
 def find_stim_artifact(subject_id):
     df_event = io.loadmat('{}/Event_Table/{}_events.mat'.format(path_CoreData, subject_id))
-    
+
     # Get unique stim anode/cathode pairs
     stim_pair_tag = np.unique([set([ev['stimAnodeTag'][0], ev['stimCathodeTag'][0]])
                                  for ev in df_event['events'][0]])
-    
+
     # Screen stimulation-related electrodes
     chan_mp_id, chan_mp_lbl, resort_ix = init_channels(subject_id)
-    
+
     chan_artifact_ix = {}
     for pair_tag in stim_pair_tag:
         pair_tag_key = '_'.join(pair_tag)
@@ -86,7 +87,7 @@ def find_stim_artifact(subject_id):
         # First, add the stimulated electrodes
         for tag in list(pair_tag):
             chan_artifact_ix[pair_tag_key].append(tag)
-            
+
         # Next, compute features related to stim artifact
         pre_stim_mean_amp = []
         post_stim_mean_amp = []
@@ -117,7 +118,7 @@ def find_stim_artifact(subject_id):
             n_samp, n_chan = evData.shape
 
             # Window the stimulation clip
-            stim_duration = ev['pulse_duration'][0, 0] / 1000.0                
+            stim_duration = ev['pulse_duration'][0, 0] / 1000.0
             n_win_dur = int(0.5*fs)
 
             n_stim_start = int(0.5*fs)
@@ -136,8 +137,8 @@ def find_stim_artifact(subject_id):
                 print('{}: {}'.format(stim_event_path, EXC))
                 continue
             pre_stim_mean_amp.append(pre_mean_amp)
-            post_stim_mean_amp.append(post_mean_amp)       
-            
+            post_stim_mean_amp.append(post_mean_amp)
+
         pre_stim_mean_amp = np.array(pre_stim_mean_amp)
         post_stim_mean_amp = np.array(post_stim_mean_amp)
 
@@ -146,5 +147,5 @@ def find_stim_artifact(subject_id):
             ts, pv = stats.ttest_rel(pre_stim_mean_amp[:, ch_ix], post_stim_mean_amp[:, ch_ix])
             if pv < (alpha / n_chan):
                 chan_artifact_ix[pair_tag_key].append(chan_mp_lbl[ch_ix])
-        
+
     return chan_artifact_ix
