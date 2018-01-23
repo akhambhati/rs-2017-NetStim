a6f9ecdbe68af165e2545eb537069b18a18a2ba0
akhambhati <akhambhati@gmail.com>
2018-01-22 19:19:54 -0500

    Modularized core function

--- Analysis_Notebooks/e02-common_avg-SGE/ee02-Measure_Adjacency.py
+++ Analysis_Notebooks/e02-common_avg-SGE/ee02-Measure_Adjacency.py
@@ -50,81 +50,89 @@ import numpy as np
 import Echobase
 #### ---------------------- Front Matter ------------------------
 
-# Grab the Task ID from QSUB
-sge_task_id = int(os.environ['SGE_TASK_ID'])-1
 
-# Load the file list
-proc_list = pkl.load(open('{}/proc_list.pkl'.format(path_ExpData), 'r'))
-pitem = proc_list[sge_task_id]
+def run(pitem):
 
-# Parse the pitem path
-base_id = '.'.join(pitem.split('/')[-1].split('.')[:-1])
-subj_id = pitem.split('/')[-1].split('.')[0]
-event_id = pitem.split('/')[-1].split('.')[-2]
+    # Parse the pitem path
+    subj_id = pitem.split('/')[-1].split('.')[0]
+    event_id = pitem.split('/')[-1].split('.')[-2]
 
-# Set output
-foutput = '{}/Adjacency.{}.npz'.format(path_ExpData, base_id)
-if os.path.exists(foutput):
-    print('Output file already exists: {}'.format(foutput))
-    sys.exit()
+    # Load data
+    try:
+        df_raw = io.loadmat(pitem)
+        df_event = io.loadmat('{}/Event_Table/{}_events.mat'.format(path_CoreData, subj_id))
+        df_monop = np.load('{}/Clean_Channels.{}.npz'.format(path_InpData, subj_id))
+    except:
+        print('Could not load raw, electrode, or event file for {}'.format(subj_id))
+        sys.exit()
 
-# Load data
-try:
-    df_raw = io.loadmat(pitem)
-    df_event = io.loadmat('{}/Event_Table/{}_events.mat'.format(path_CoreData, subj_id))
-    df_monop = np.load('{}/Clean_Channels.{}.npz'.format(path_InpData, subj_id))
-except:
-    print('Could not load raw, electrode, or event file for {}'.format(subj_id))
-    sys.exit()
+    # Get event data
+    event = df_event['events'][0, int(event_id)-1]
+    if event['type'][0] == 'STIMULATING':
+        stim_duration = event['pulse_duration'][0, 0] / 1000.0
+    elif event['type'][0] == 'SHAM':
+        event_stim_ix = np.flatnonzero(df_event['events']['type'][0, :] == 'STIMULATING')
+        closest_event = np.argmin(np.abs(event_stim_ix - (int(event_id)-1)))
+        stim_duration = df_event['events'][0, closest_event]['pulse_duration'][0, 0] / 1000.0
+    else:
+        print('Invalid Event Type: {}'.format(event['type'][0]))
+        sys.exit()
 
-# Retrieve data
-evData = df_raw['evData'][...][df_monop['monopolar_resort_ix'], :].T
-fs = int(np.ceil(df_raw['Fs'][0, 0]))
-n_samp, n_chan = evData.shape
+    stim_anode_tag = event['stimAnodeTag'][0]
+    stim_cathode_tag = event['stimCathodeTag'][0]
+    stim_pair_tag = '_'.join({stim_anode_tag, stim_cathode_tag})
 
-# Retrieve event and relevant information
-event = df_event['events'][0, int(event_id)-1]
+    # Retrieve data
+    evData = df_raw['evData'][...][df_monop['monopolar_resort_ix'], :].T
+    fs = int(np.ceil(df_raw['Fs'][0, 0]))
+    n_samp, n_chan = evData.shape
 
-if not (event['type'][0] in ['STIMULATING', 'SHAM']):
-    print('Invalid Event Type: {}'.format(event['type'][0]))
-    sys.exit()
+    # Remove artifactual channels for this stim location
+    goodchan_ix = np.setdiff1d(np.arange(n_chan), df_monop['monopolar_id_artifact'][()][stim_pair_tag])
+    evData = evData[:, goodchan_ix]
 
-if event['type'][0] == 'STIMULATING':
-    stim_duration = event['pulse_duration'][0, 0] / 1000.0
+    # Window the stimulation clip
+    n_win_dur = int(0.5*fs)
 
-if event['type'][0] == 'SHAM':
-    event_stim_ix = np.flatnonzero(df_event['events']['type'][0, :] == 'STIMULATING')
-    closest_event = np.argmin(np.abs(event_stim_ix - (int(event_id)-1)))
-    stim_duration = df_event['events'][0, closest_event]['pulse_duration'][0, 0] / 1000.0
+    n_stim_start = int(0.5*fs)
+    n_stim_dur = int(stim_duration*fs)
+    n_stim_end = n_stim_start + n_stim_dur
+    n_stim_pad = int(100/1000.0*fs)
 
-stim_anode_tag = event['stimAnodeTag'][0]
-stim_cathode_tag = event['stimCathodeTag'][0]
-stim_pair_tag = '_'.join({stim_anode_tag, stim_cathode_tag})
+    win_pre_stim = list(np.arange(n_stim_start-n_win_dur, n_stim_start))
+    win_post_stim = list(np.arange(n_stim_end+n_stim_pad, n_stim_end+n_stim_pad+n_win_dur))
 
-# Remove artifactual channels for this stim location
-goodchan_ix = np.setdiff1d(np.arange(n_chan), df_monop[stim_pair_tag])
-evData = evData[:, goodchan_ix]
+    # Formulate output dictionary
+    adj = {'Pre_Stim': {}, 'Post_Stim': {}}
 
-# Window the stimulation clip
-n_win_dur = int(0.5*fs)
+    # Compute Pre-Stim Adjacency
+    adj['Pre_Stim']['AlphaTheta'], adj['Pre_Stim']['Beta'], adj['Pre_Stim']['LowGamma'], adj['Pre_Stim']['HighGamma'] = Echobase.Pipelines.ecog_network.multiband_conn(evData[win_pre_stim, :], fs, avgref=True)
 
-n_stim_start = int(0.5*fs)
-n_stim_dur = int(stim_duration*fs)
-n_stim_end = n_stim_start + n_stim_dur
-n_stim_pad = int(100/1000.0*fs)
+    # Compute Pre-Stim Adjacency
+    adj['Post_Stim']['AlphaTheta'], adj['Post_Stim']['Beta'], adj['Post_Stim']['LowGamma'], adj['Post_Stim']['HighGamma'] = Echobase.Pipelines.ecog_network.multiband_conn(evData[win_post_stim, :], fs, avgref=True)
 
-win_pre_stim = list(np.arange(n_stim_start-n_win_dur, n_stim_start))
-win_post_stim = list(np.arange(n_stim_end+n_stim_pad, n_stim_end+n_stim_pad+n_win_dur))
+    return adj
 
-# Formulate output dictionary
-adj = {'Pre_Stim': {}, 'Post_Stim': {}}
 
-# Compute Pre-Stim Adjacency
-adj['Pre_Stim']['AlphaTheta'], adj['Pre_Stim']['Beta'], adj['Pre_Stim']['LowGamma'], adj['Pre_Stim']['HighGamma'] = Echobase.Pipelines.ecog_network.multiband_conn(evData_mp[win_pre_stim, :], fs, avgref=True)
+if __name__ == '__main__':
+    # Grab the Task ID from QSUB
+    sge_task_id = int(os.environ['SGE_TASK_ID'])-1
 
-# Compute Pre-Stim Adjacency
-adj['Post_Stim']['AlphaTheta'], adj['Post_Stim']['Beta'], adj['Post_Stim']['LowGamma'], adj['Post_Stim']['HighGamma'] = Echobase.Pipelines.ecog_network.multiband_conn(evData_mp[win_post_stim, :], fs, avgref=True)
+    # Load the file list
+    proc_list = pkl.load(open('{}/proc_list.pkl'.format(path_ExpData), 'r'))
+    pitem = proc_list[sge_task_id]
 
-# Save the output
-np.savez(foutput, adj=adj)
+    # Parse the pitem path
+    base_id = '.'.join(pitem.split('/')[-1].split('.')[:-1])
+
+    # Set output
+    foutput = '{}/Adjacency.{}.npz'.format(path_ExpData, base_id)
+    if os.path.exists(foutput):
+        print('Output file already exists: {}'.format(foutput))
+        sys.exit()
+
+    adj = run(pitem)
+
+    # Save the output
+    np.savez(foutput, adj=adj)
 "
